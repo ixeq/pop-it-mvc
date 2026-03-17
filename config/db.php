fce0d264c61e54535eb6f3777131a5d8fe560797
ixeq <tim.matyskin@gmail.com>
2026-03-17 18:27:05 +0700

    Функционал Аунтификация и Регестрация

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'sybsrdmc_m3',
+    'username' => 'sybsrdmc',
+    'password' => 'FLDcjP',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
--- core/Src/Route.php
+++ core/Src/Route.php
@@ -42,6 +42,8 @@ class Route
         }
 
 
-        call_user_func([new $class, $action]);
+        call_user_func([new $class, $action], new Request());
+
+
     }
 }
